bde967073a28c48d3d0b233cc446cfec70e6ef10
Nguyen Van Hau <nguyenvanhau.soft@gmail.com>
2021-10-17 02:20:46 +0700

    change app to admin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('app', function () {
-    return view('app');
+Route::get('admin', function () {
+    return view('admin');
 });
 
